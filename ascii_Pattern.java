0d67a0c4ef890883373b83a4c9f1e935b6963e44
helsnzh123 <63345900+helsnzh123@users.noreply.github.com>
2021-06-10 11:22:09 +0530

    Updated ASCII Pattern - 1

--- ascii_Pattern.java
+++ ascii_Pattern.java
@@ -1,39 +1,86 @@
 package assignment_1;
 
-class ASCII{
-	public final static int size = 1;
-	public void upper_line(){
-		System.out.print("+");
-		for(int i = 1;i<=size * 2;i++) {
-			System.out.print("-");
-		}
-		System.out.print("+ \n");
-	}
-	public void top_half() {
-		if(size==1) {
-			System.out.print("|<>|\n");
-			System.out.print("+");
-			for(int i = 1;i<=size * 2;i++) {
-				System.out.print("-");
-			}
-			System.out.print("+");
-			}
-			else {
-				int i = 1;
-				for(i = 1;i<=size;i++) {
-					System.out.println("|");
-				}
-			}
-		}
-	}
 
 
 public class ascii_Pattern {
-	public static void main(String[] args) {
-		ASCII p1 = new ASCII();
-		p1.upper_line();
-		p1.top_half();
+	static void top_Bottom_Line(int size) {
+		System.out.print("+");
+		for(int i = 0;i<(size)*2;i++) {
+			System.out.print("-");
+		}
+		System.out.println("+");
+	}
+	static void top_half(int size) {
+		for(int row=0;row<size-1;row++) {
+			System.out.print("|");
+			for(int i=0;i<size-row-1;i++) {
+				System.out.print(" ");
+			}
+			System.out.print("/");
+			for(int i=0;i<(2*row);i++){
+				if(row%2!=0)
+					System.out.print("-");
+					else 
+						System.out.print("=");
+			}
+			System.out.print("\\");
+			for(int i=0;i<size-row-1;i++) {
+				System.out.print(" ");
+			}
+			System.out.print("|\n");
+		}
 		
+	}
+	static void middle_line(int size) {
+		System.out.print("|");
+		System.out.print("<");
+		if(size%2==0) {
+			for(int i = 0;i<(2*size - 2);i++) {
+				System.out.print("-");
+			}}
+			else {
+				for(int i = 0;i<(2*size - 2);i++)
+				System.out.print("=");
+			}
+			
+		System.out.print(">");
+		System.out.println("|");
+	}
+	static void bottom_half(int size) {
+		 for(int row=size-2; row>=0; row--) {
+	            System.out.print("|");
+	            for(int i=0; i<size-row-1;i++) {
+	                System.out.print(" ");
+	            }
+	            System.out.print("\\");
+
+	            for(int i=0; i<2*row;i++) {
+	                if(row%2==0)
+	                    System.out.print("=");
+	                else
+	                    System.out.print("-");
+	            }
+
+	            System.out.print("/");
+
+	            for(int i=0; i<size-row-1;i++) {
+	                System.out.print(" ");
+	            }
+	            System.out.println("|");
+	            }
+	}
+	
+	static void artistic_pattern(int size) {
+		top_Bottom_Line(size);
+		top_half(size);
+		middle_line(size);
+		bottom_half(size);
+		top_Bottom_Line(size);
+	}
+	public static void main(String[] args) {
+		
+	
+		artistic_pattern(7);
 
 	}
 
